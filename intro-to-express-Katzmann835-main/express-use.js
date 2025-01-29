1b3b38e3ccc74f5cbe2b621ed6e6671f5f1c96b9
katzmann835 <katzmann@umich.edu>
2025-01-28 21:22:00 -0500

    Minor fixes

--- intro-to-express-Katzmann835-main/express-use.js
+++ intro-to-express-Katzmann835-main/express-use.js
@@ -5,31 +5,31 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.sendfile(__dirname + '/New-website.html');
+    res.sendFile(__dirname + '/New-website.html');
 })
 
 app.get('/new-web.css', (req, res) => {
-    res.sendfile(__dirname + '/new-web.css');
+    res.sendFile(__dirname + '/new-web.css');
 })
 
 app.get('/map-of-oregon-cities.jpeg', (req, res) => {
-    res.sendfile(__dirname + '/map-of-oregon-cities.jpeg');
+    res.sendFile(__dirname + '/map-of-oregon-cities.jpeg');
 })
 
 app.get('/israel_map.jpeg', (req, res) => {
-    res.sendfile(__dirname + '/israel_map.jpeg');
+    res.sendFile(__dirname + '/israel_map.jpeg');
 })
 
 app.get('/Costa-Rica-Map.jpeg', (req, res) => {
-    res.sendfile(__dirname + '/Costa-Rica-Map.jpeg');
+    res.sendFile(__dirname + '/Costa-Rica-Map.jpeg');
 })
 
 app.get('/Alaska_map-L.jpeg', (req, res) => {
-    res.sendfile(__dirname + '/Alaska_map-L.jpeg');
+    res.sendFile(__dirname + '/Alaska_map-L.jpeg');
 })
 
 app.get('/Hawaii-map-M.jpeg', (req, res) => {
-    res.sendfile(__dirname + '/Hawaii-map-M.jpeg');
+    res.sendFile(__dirname + '/Hawaii-map-M.jpeg');
 })
 
 app.listen(port, () => {
