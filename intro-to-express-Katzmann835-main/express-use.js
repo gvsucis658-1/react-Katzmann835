2da97d68fcd6f92386dd4fb45b70ccdbf9d75b30
katzmann835 <katzmann@umich.edu>
2025-01-28 21:15:43 -0500

    Static webpage works

--- intro-to-express-Katzmann835-main/express-use.js
+++ intro-to-express-Katzmann835-main/express-use.js
@@ -1,1 +1,40 @@
-//This will create the application using the expressjs feature
+//This creates an js file which can run the static and dymamic websites
+//Running the static webpage:
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.sendfile(__dirname + '/New-website.html');
+})
+
+app.get('/new-web.css', (req, res) => {
+    res.sendfile(__dirname + '/new-web.css');
+})
+
+app.get('/map-of-oregon-cities.jpeg', (req, res) => {
+    res.sendfile(__dirname + '/map-of-oregon-cities.jpeg');
+})
+
+app.get('/israel_map.jpeg', (req, res) => {
+    res.sendfile(__dirname + '/israel_map.jpeg');
+})
+
+app.get('/Costa-Rica-Map.jpeg', (req, res) => {
+    res.sendfile(__dirname + '/Costa-Rica-Map.jpeg');
+})
+
+app.get('/Alaska_map-L.jpeg', (req, res) => {
+    res.sendfile(__dirname + '/Alaska_map-L.jpeg');
+})
+
+app.get('/Hawaii-map-M.jpeg', (req, res) => {
+    res.sendfile(__dirname + '/Hawaii-map-M.jpeg');
+})
+
+app.listen(port, () => {
+    console.log('Server listening on port${port}')
+})
+
+
+//Running the dynamic webpage:
