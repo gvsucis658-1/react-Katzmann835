64293d8b789a0184dd599d9672f3d7467d087d71
katzmann835 <katzmann@umich.edu>
2025-01-30 21:10:08 -0500

    Trouble getting the dynamic webpage running

--- intro-to-express-Katzmann835-main/express-use.js
+++ intro-to-express-Katzmann835-main/express-use.js
@@ -33,35 +33,28 @@ app.get('/Hawaii-map-M.jpeg', (req, res) => {
     res.sendFile(__dirname + '/Hawaii-map-M.jpeg');
 });
 
-app.get('/Dynamic-web.ejs', (req, res) => {
-    res.render(__dirname + '/Dynamic-web.ejs');
-});
+//Running the dynamic webpage:
 
-app.get('/', (req, res) => {
+app.get('/Dynamic-web', (req, res) => {
     const data = {
         weather: 'Finding the Weather',
         traveling: 'traveling',
         temp: ['45', '55', '66', '3', '77'],
         dist: ['2281.9', '6084', '3639.7', '2989', '4291']
     }
-    res.render('Dynamic-web.ejs', data);
+    res.render('Dynamic-web', data);
 });
 
 app.post('/submit', (req, res) => {
-    const Weather = req.body.Weather;
-    const Travel = req.body.Travel;
     const data = {
         weather: 'The weather is:',
         traveling: 'The travel distance is:',
-        temp: 'The temperatore is ${temp} degrees.',
-        dist: 'The total distance from Grand Rapids to the distination is: ${dist} miles.'
+        temp: ['45', '55', '66', '3', '77'],
+        dist: ['2281.9', '6084', '3639.7', '2989', '4291']
     }
-    res.render('Dynamic-web.ejs', data);
+    res.render('Dynamic-web', data);
 })
 
 app.listen(port, () => {
     console.log('Server listening on port${port}')
 });
-
-
-//Running the dynamic webpage:
