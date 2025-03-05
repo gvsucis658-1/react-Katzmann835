00d6ccc4dc128c5d9ab9fd5e9743707bcb4e7d16
katzmann835 <katzmann@umich.edu>
2025-03-04 21:05:01 -0500

    Current work on DOM manipulation

---
+++ DOM_work/DOM_work.js
@@ -0,0 +1,27 @@
+const One = document.getElementsByClassName('Red_square')
+const Total_One = One.length
+for (let i = 0; i < One.length; i++){
+    One[i].style.backgroundcolor = 'red'
+}
+
+const Two = document.getElementsByClassName('Yellow_square')
+const Total_Two = Two.length
+for (let i = 0; i < Two.length; i++){
+    Two[i].style.backgroundcolor = 'yellow'
+    Two[i].style.color = 'black'
+}
+
+const Three = document.getElementsByClassName('Indigo_square')
+const Total_Three = Three.length
+for (let i = 0; i < Three.length; i++){
+    Three[i].style.backgroundcolor = 'indigo'
+    Three[1].style.color = 'white'
+}
+
+const Four = document.getElementsByClassName('Magenta_square')
+const Total_Four = Four.length
+for (let i = 0; i < Four.length; i++){
+    Four[i].style.backgroundcolor = 'magenta'
+}
+
+
