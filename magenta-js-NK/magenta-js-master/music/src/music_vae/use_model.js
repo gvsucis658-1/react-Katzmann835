ee2e78990d879a4cbfad8b9ef072d34c9709ebe8
katzmann835 <katzmann@umich.edu>
2025-04-10 09:21:41 -0400

    The Model is almost functional

--- magenta-js-NK/magenta-js-master/music/src/music_vae/use_model.js
+++ magenta-js-NK/magenta-js-master/music/src/music_vae/use_model.js
@@ -1,70 +1,79 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.Call_Music = Call_Music;
+exports.Mel_prompt = Mel_prompt;
+exports.store_Mel = store_Mel;
+exports.retrieve_Mel = retrieve_Mel;
+exports.start_Mel = start_Mel;
+exports.time_Mel = time_Mel;
+exports.stop_Mel = stop_Mel;
 //Psuedocode for new code to ensure the user can choose and create their own music
 //This is based on the usercase workflow chart I created to ensure that what I made is functional
-import {MusicVAE} from "./model";
-import promptSync from "prompt-sync";
-import Melody from "../core/melodies";
-//TODO: the user must be able to type a melody prompt
-const prompt = promptSync();
-let input = prompt("Type a Melody prompt: "); //This will be used to call the melody prompt
+import { MusicVAE } from './model';
+import { Melody } from '../core/melodies';
+
+let currentMel = null;
 //TODO: Make a page that has a function that can call any melody from the code.
-function Call_Music() {
-    console.log(MusicVAE);
+
+function Call_Music(input) {
+    console.log("Calls the melody from the function", input);
+    currentMel = input;
 }
 //TODO: Once this is done, create a prompt function such that the user can call get the melody from the function such that the user can run the melody
-function Mel_prompt() {
-    ChosenPrompt = input(Melody);
-    console.log(`Melody '${ChosenPrompt}' has been chosen and ready to be prompted`)
+function Mel_prompt(input) {
+    console.log("Gets the melody from prompt", input);
+    currentMel = input;
 }
 //TODO: Once the user types the prompt, there needs to be a function such that it stores the components of a melody. This should be a seperate page 
 //such that the page contains a module which will consist of functions which will take from functions that provide the melodies, and then create a 
 //function that stores the melody
 function store_Mel() {
-    const melData = JSON.stringify(Melody);
-    localStorage.setItem('StoredMelody', melData);
-    console.log("Melody has been stored")
+    if (currentMel) {
+        const StoreTheMel = JSON.stringify(Melody);
+        localStorage.setItem("Melody_stored", StoreTheMel);
+        console.log("Melody_stored", StoreTheMel);
+    }
+    else {
+        console.log("Melody is unable to be stored");
+    }
 }
 //TODO: After the function is stored and the melody is prompted, the code should go through the page, and take the melody from the store module to the start function
 //Which will be on a different page or module
 function retrieve_Mel() {
-    const melodyStored = localStorage.getItem('StoredMelody');
-    if (melodyStored){
-        const melody = JSON.parse(melodyStored);
-        console.log("Melody has been retrieved from storage");
-        return melody;
+    const Mel_stored = localStorage.getItem("Melody_stored");
+    if(Mel_stored){
+        const errorMel = JSON.parse(Mel_stored);
+        currentMel = errorMel;
+        console.log("Get the Melody", errorMel);
+        return errorMel;
+    } else { 
+        return null;
     }
-    Melody("Melody failed to retrieve the melody");
-    return null;
 }
 //TODO: Once the melody enters the start function located on a new page or module, the function will start the new melody prompted by the user
-function start_Mel() {
-    var Melody = retrieve_Mel();
-    if (Melody){
-        Melody.start();
-        console.log("Melody has started")
-    } else {
-        console.log("No Melody has been started")
-    }
+async function start_Mel() {
+    const NewMel = retrieve_Mel();
+    if(NewMel) {
+        try{
+            await MusicVAE.apply(currentMel);
+            await Melody.apply(currentMel);
+
+        }
+        catch{
+            console.log("Melody has not started");
+        }
+    };
 }
 //TODO: After the melody starts, the melody will play for a specific time. There needs to be a function that will determine how long the melody will play when the function is executed
-function time_Mel() {
-
-    setTimeout(() => {
-        console.log("Did the code stop?", 1000)
-        console.log("Did the code stop?", 5000);
-        console.log("Did the code stop?", 10000);
-    })
+function time_Mel(duration) {
+    console.log(`Melody will play for: ${duration} seconds`);
+    setTimeout(function () {
+        console.log("The melody has ended");
+        stop_Mel();
+    }, duration * 1000);
     //This function will be used to stop the time as provided by the user
 }
 //TODO: Finally, the function will have an end condition which is when the melody will stop playing. 
 function stop_Mel() {
-    setTimeout(() => {
-        console.log("Melody has stopped");
-        const stopMel = retrieve_Mel();
-        if (stopMel && stopMel.stop) {
-            stopMel.stop();
-        }
-    }, 1000);
+    console.log("Melody has ended");
 }
-console.log(stop_Mel);
--- magenta-js-NK/magenta-js-master/music/src/music_vae/use_model.ts
+++ magenta-js-NK/magenta-js-master/music/src/music_vae/use_model.ts
@@ -1,60 +1,69 @@
 //Psuedocode for new code to ensure the user can choose and create their own music
 //This is based on the usercase workflow chart I created to ensure that what I made is functional
-import {MusicVAE} from './model';
-import promptsync from 'prompt-sync';
+import { MusicVAE } from './model';
 import { Melody } from '../core/melodies';
 
-
-//TODO: the user must be able to type a melody prompt
-const prompt = promptsync(MusicVAE);
-const input: string = prompt(MusicVAE); //This will be used to call the melody prompt
+let currentMel: string | null = null;
 
 //TODO: Make a page that has a function that can call any melody from the code.
-function Call_Music(){
-    console.log("Calls the melody from the function");
+export function Call_Music(input: string): void{
+    console.log("Calls the melody from the function", input);
+    currentMel = input;
 }
 
 
 //TODO: Once this is done, create a prompt function such that the user can call get the melody from the function such that the user can run the melody
-function Mel_prompt(){
-    prompt(Melody)
+export function Mel_prompt(input: string): void{
+    console.log("Gets the melody from prompt", input);
+    currentMel = input;
 }
 
 //TODO: Once the user types the prompt, there needs to be a function such that it stores the components of a melody. This should be a seperate page 
 //such that the page contains a module which will consist of functions which will take from functions that provide the melodies, and then create a 
 //function that stores the melody
 
-function store_Mel(){
-    Call_Music.toString();
-    localStorage.setItem(MusicVAE.toString(), JSON.stringify('Melody'));
+export function store_Mel(): void{
+    if(currentMel){
+        localStorage.setItem("Melody_stored", currentMel);
+        console.log("Melody_stored", currentMel);
+    } else {
+        console.log("Melody is unable to be stored");
+    }
 }
 
 
 //TODO: After the function is stored and the melody is prompted, the code should go through the page, and take the melody from the store module to the start function
 //Which will be on a different page or module
-function retrieve_Mel(){
-    const Melody = localStorage.getItem(MusicVAE.toString());
+export function retrieve_Mel(): string | null {
+    const Mel_stored = localStorage.getItem("Melody_stored");
+    console.log("Get_Melody", Mel_stored);
+    currentMel = Mel_stored;
+    return Mel_stored;
 }
 
 
 //TODO: Once the melody enters the start function located on a new page or module, the function will start the new melody prompted by the user
-function start_Mel(){
-    let Melody = performance.now();
+export function start_Mel(){
+    const NewMel = performance.now();
+    MusicVAE.apply(currentMel);
+    Melody.apply(currentMel);
+    
 }
 
 //TODO: After the melody starts, the melody will play for a specific time. There needs to be a function that will determine how long the melody will play when the function is executed
-function time_Mel(){
+export function time_Mel(duration: number){
 
-    Melody.call(console.log("Did the code stop?", 1000));
-    Melody.call(console.log("Did the code stop?", 5000));
-    Melody.call(console.log("Did the code stop?", 10000));
+    console.log(`Melody will play for:${duration} seconds`);
+
+    setTimeout(() => {
+        console.log("The melody has ended");
+        stop_Mel();
+    }, duration);
     //This function will be used to stop the time as provided by the user
 }
 
 
 //TODO: Finally, the function will have an end condition which is when the melody will stop playing. 
-function stop_Mel(){
-    /*Melody.peformance(() => {
-        setTimeout
-    }, 1000);*/
+export function stop_Mel(){
+    console.log ("Melody has ended");
 }
